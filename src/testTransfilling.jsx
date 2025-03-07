ae886a276a81aef274bdb3b6fa4d0996f5a631ac
GitHubJIYEON <jiyeon2954@gmail.com>
2025-03-07 15:14:55 +0900

    Feat: package.json "babel" 추가, sample.jsx삭제 testTransfiling.jsx 추가
    - "babel": "npx babel --plugins @babel/plugin-transform-react-jsx"

---
+++ src/testTransfilling.jsx
@@ -0,0 +1,12 @@
+export default function testTransfilling() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+// $ npm run babel ./src/testTransfilling.jsx
+// export default function testTransfilling() {
+//   return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello World"));
+// }
