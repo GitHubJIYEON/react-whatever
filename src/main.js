d0c2b4b715fb6cabec7cd0952445c2234d7e9e1d
GitHubJIYEON <jiyeon2954@gmail.com>
2025-03-10 23:58:29 +0900

    Feat: build 후 dist/App.js  console.log 확인

--- src/main.js
+++ src/main.js
@@ -1,50 +1,8 @@
 import "./style.css";
-import App from "./App.transpiled.js";
+import App from "../dist/App.js";
 import { createElement } from "./createElement.js";
 
 window.createElement = createElement;
 const appElement = App();
 
 console.log(JSON.stringify(appElement, null, 2));
-
-// console.log 결과
-
-// {
-//   "type": "div",
-//   "props": {
-//     "id": "app",
-//     "children": [
-//       {
-//         "type": "h1",
-//         "props": {
-//           "children": "Hello world"
-//         }
-//       },
-//       {
-//         "type": "p",
-//         "props": {
-//           "children": "custom Virtual DOM!"
-//         }
-//       },
-//       {
-//         "type": "ul",
-//         "props": {
-//           "children": [
-//             {
-//               "type": "li",
-//               "props": {
-//                 "children": "Javascript"
-//               }
-//             },
-//             {
-//               "type": "li",
-//               "props": {
-//                 "children": "React"
-//               }
-//             }
-//           ]
-//         }
-//       }
-//     ]
-//   }
-// }
